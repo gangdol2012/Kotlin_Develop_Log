8008beaf7fcd0a91131198ce2f67d984cc2a5477
gangdol2012 <gangdol2012@gmail.com>
2023-09-20 00:18:37 +0900

    Inital commit

---
+++ chajajue/app/src/main/java/com/spacex/chajajue/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.spacex.chajajue
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
